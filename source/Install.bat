c39eaff27202fefa9b776ce60a21c6687f233db4
BB pezsgő <bazsi5fel2@gmail.com>
2023-03-01 06:10:16 +0100

    Delete Install.bat

--- source/Install.bat
+++
@@ -1,2 +0,0 @@
-npm i asciichart discord.js ffmpeg-static moonphase-js weather-js nodejs-weather-app jsdom @discordjs/voice ytdl-core axios prism-media express express-handlebars winston winston-daily-rotate-file
-pause
